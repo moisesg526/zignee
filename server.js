d0fff112f48093c98b7e6b7ffea6f710ab726dd8
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-05-12 23:45:52 -0400

    Added middleware to server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/database");
+
+require("dotenv").config({ path: "./config/.env" });
+
+connectDB();
+
+// Tells express to use EJS as the templating engine
+app.set("view engine", "ejs");
+// Serves static files from public directory. ex. CSS, JS, images
+app.use(express.static("public"));
+// Lets you access form data via req.body
+app.use(express.urlencoded({ extended: true }));
+// Parses incoming requests with application/json
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
