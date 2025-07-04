7cfbdf0726be62ed3860dd3350b1fbbe177a3857
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2025-07-04 12:40:11 -0400

    Got the current date to dislpay in dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,6 +2,21 @@ const User = require("../models/User");
 
 module.exports = {
   getDashboard: async (req, res) => {
-    res.render("customerDashboard.ejs", { name: req.name });
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const currentMonth = monthNames[new Date().getMonth()];
+    res.render("customerDashboard.ejs", { name: req.name, currentMonth });
   },
 };
